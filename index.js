bb03ab3a4bffc2068461434b78302ea047d0e3e3
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2021-06-24 17:53:56 +0600

    1753

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = (number, words, concat) => {
       number % 100 > 4 && number % 100 < 20
         ? 2
         : [2, 0, 1, 1, 1, 2][Math.min(number % 10, 5)]
-    ];
+    ]
 
-  return concat ? number + ' ' + result : result;
-};
+  return concat ? number + ' ' + result : result
+}
